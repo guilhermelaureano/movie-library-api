007ca220f5cfb91bf6ae3b557f3d732eae145d39
Guilherme Laureano <guilhermeml001@gmail.com>
2023-07-27 00:07:35 -0300

    change port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger: console });
-  await app.listen(3000);
+  await app.listen(3002);
 }
 bootstrap();
